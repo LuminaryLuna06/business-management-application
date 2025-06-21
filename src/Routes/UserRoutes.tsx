0ae34b7826ac86c37f541f244fe246d0e733b832
Luna <maithetranh@gmail.com>
2025-06-21 10:48:05 +0700

    chore: finish Routes

--- src/Routes/UserRoutes.tsx
+++ src/Routes/UserRoutes.tsx
@@ -3,6 +3,11 @@ import ProtectedRoutes from "./ProtectedRoutes";
 import HomePage from "../pages/Home";
 import TestPage from "../pages/test";
 import BusinessPage from "../pages/Business";
+import EmployeesPage from "../pages/Business/components/Employees";
+import SubLicenses from "../pages/Business/components/Sub-Licenses";
+import DashboardPage from "../pages/Business/components/Dashboard";
+import InspectionPage from "../pages/Business/components/Inspection-Schedule";
+import ViolationPage from "../pages/Business/components/Violations";
 
 export default function UserRoutes() {
   return (
@@ -11,14 +16,11 @@ export default function UserRoutes() {
         <Route path="/" element={<HomePage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="/business/:businessId" element={<BusinessPage />}>
-          <Route path="dashboard" element={<div>Dashboard Page</div>} />
-          <Route path="employees" element={<div>Employees Page</div>} />
-          <Route path="sub-licenses" element={<div>Sublicense Page</div>} />
-          <Route
-            path="inspection-schedule"
-            element={<div>Inspection Page</div>}
-          />
-          <Route path="violations" element={<div>Violation Page</div>} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="employees" element={<EmployeesPage />} />
+          <Route path="sub-licenses" element={<SubLicenses />} />
+          <Route path="inspection-schedule" element={<InspectionPage />} />
+          <Route path="violations" element={<ViolationPage />} />
         </Route>
       </Route>
     </Routes>

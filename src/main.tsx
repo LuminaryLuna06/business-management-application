24654b53c3c4312ce06d54264d990093e3e9466a
Luna <maithetranh@gmail.com>
2025-06-24 10:45:07 +0700

    chore: delete Strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import "@mantine/charts/styles.css";
 import "@mantine/dates/styles.css";
 import "mantine-react-table/styles.css";
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MantineProvider defaultColorScheme="light">
-      <App />
-    </MantineProvider>
-  </StrictMode>
+  <MantineProvider defaultColorScheme="light">
+    <App />
+  </MantineProvider>
 );

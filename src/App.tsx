e78572cfdf6a642be72ca03a4c1692a4f9ce8c80
Luna <maithetranh@gmail.com>
2025-06-30 00:25:17 +0700

    feat: add notification

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { HashRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./context/authContext";
-// import UserRoutes from "./Routes/UserRoutes";
+import { Notifications } from "@mantine/notifications";
 import PublicRoutes from "./Routes/PublicRoutes";
 import AdminRoutes from "./Routes/AdminRoutes";
+import "dayjs/locale/vi";
+
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
+      <Notifications position="top-right" />
       <Router>
         <AuthProvider>
-          {/* <UserRoutes /> */}
           <AdminRoutes />
         </AuthProvider>
         <PublicRoutes />

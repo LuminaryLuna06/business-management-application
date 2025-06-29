b9ccab5409bdd93c3f052c82f5918cf74a3b2a16
Luna <maithetranh@gmail.com>
2025-06-30 00:25:30 +0700

    feat: theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,27 @@ import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
 import "@mantine/dates/styles.css";
 import "mantine-react-table/styles.css";
+import "@mantine/notifications/styles.css";
 import { ModalsProvider } from "@mantine/modals";
 import { createRoot } from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import App from "./App.tsx";
+import { DateInput } from "@mantine/dates";
+import "dayjs/locale/vi";
+
+const theme = createTheme({
+  components: {
+    DateInput: DateInput.extend({
+      defaultProps: {
+        locale: "vi",
+        valueFormat: "DD [Tháng] MM, YYYY",
+      },
+    }),
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <MantineProvider defaultColorScheme="light">
+  <MantineProvider defaultColorScheme="light" theme={theme}>
     <ModalsProvider>
       <App />
     </ModalsProvider>
